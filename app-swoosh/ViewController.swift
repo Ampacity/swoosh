483700d5b6aee28377cbde02ccb52a996096c9f6
Solid <solid@solids-macbook-air.local>
2018-12-12 19:57:38 +0300

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Solid on 12/12/18.
+//  Copyright © 2018 Solid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
